980bcacc33edab71ef694bfe122316750e7babab
Jihyun-Choi <70438098+Jihyun-Choi@users.noreply.github.com>
2021-08-07 22:54:27 +0900

    create join accept

--- teamapp/models.py
+++ teamapp/models.py
@@ -10,14 +10,13 @@ class Team(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='team')
 
     # member를 다룰 수 있는 자료형으로 반복문 돌릴 수 있도록
-    member1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='team')
-    member2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='team')
-    member3 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='team')
-    member4 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='team')
-    member5 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='team')
-    fail = models.ForeignKey(User, on_delete=models.CASCADE, related_name='team')
+    member1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='team_1', null=True)
+    member2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='team_2', null=True)
+    member3 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='team_3', null=True)
+    member4 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='team_4', null=True)
+    member5 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='team_5', null=True)
 
-
+    #count = models.IntegerField(default=1, null=True)
     # 하나의 user가 프로젝트를 한번만 team할 수 있도록
     class Meta:
         unique_together = ('user', 'project')
--- teamapp/views.py
+++ teamapp/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -19,13 +20,44 @@ class TeamView(RedirectView):
         return reverse('projectapp:detail', kwargs={'pk': self.request.GET.get('project_pk')})
 
     def get(self, request, *args, **kwargs):
+        #전달받은 pk를 이용해 user, project객체를 특정하고 이를 필터링하는데 사용
+        user = get_object_or_404(User, pk=self.request.GET.get('user'))
         project = get_object_or_404(Project, pk=self.request.GET.get('project_pk'))
-        user = self.request.user
-        team = Team.objects.filter(user=user, project=project)
+        join = Join.objects.filter(user=user, project=project)
 
-        if Team.exists():
-            team.delete()
-        else:
-            Team(user=user, project=project).save()
+        if join.exists():
+            join.delete()
+
+
+        #필터링 된 팀 객체를 변수에 받아옴
+        team = Team.objects.filter(user=project.writer, project=project)
+
+        #user, project에 맞는 team객체가 없다면 새로 생성
+        if not(team.exists()):
+            Team(user=project.writer, project=project).save()
+
+
+        for object in team:
+            #if object.project.title == project.title:
+                if object.member1 == None:
+                    object.member1 = user
+                elif object.member2 == None:
+                    object.member2 = user
+                elif object.member3 == None:
+                    object.member3 = user
+                elif object.member4 == None:
+                    object.member4 = user
+                elif object.member5 == None:
+                    object.member5 = user
+                else:
+                    pass
+                #elif object.member3 == None:
+                #    object.member3 = user
+                #elif object.member4 == None:
+                #    object.member4 = user
+                #elif object.member5 == None:
+                #    object.member5 = user
+
+                object.save()
 
         return super(TeamView, self).get(request, *args, **kwargs)
